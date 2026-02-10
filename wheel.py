9f8fca4badd116b2ffde6b41b36a7b827b4720c3
amark2005 <amarkeshav14@gmail.com>
2026-02-10 13:04:50 +0530

    fix error

--- wheel.py
+++ wheel.py
@@ -1,8 +1,12 @@
 import random as rand
 class wheel:
-    data=list()
+    data=[1,2,3,4]
+    l3=list()
+    l3_result=None
+    finale=list()
     def __init__(self):
-        self.getdata()
+        #self.getdata()
+        self.level3()
         pass
     def getdata(self):
         while True:
@@ -13,28 +17,24 @@ class wheel:
             elif x == None:
                 a=0
                 pass
-wheel()def getdata(self):
+    def getdata(self):
         while True:
-            a=1
             x=input("Enter the datas: ")
-            if a == 1:
-                self.data.append(x)
-            elif x == None:
-                a=0
-                passdef getdata(self):
-        while True:
-            a=1
-            x=input("Enter the datas: ")
-            if a == 1:
-                self.data.append(x)
-            elif x == None:
-                a=0
-                passdef getdata(self):
-        while True:
-            a=1
-            x=input("Enter the datas: ")
-            if a == 1:
-                self.data.append(x)
-            elif x == None:
-                a=0
-                pass
+            if not x:
+                break
+            self.data.append(x)
+        pass
+    def level3(self):
+        for i in range(0,rand.randint(30,50)):
+            self.l3.append(rand.choice(self.data))
+        print(self.l3)
+        self.l3_result=max(set(self.l3),key=self.l3.count)
+        self.l3_lose=min(set(self.l3),key=self.l3.count)
+        self.finale.append(self.l3_result)
+        self.
+        print(f"Level 3 Winner is {self.l3_result}")
+        
+        
+        
+
+wheel()
