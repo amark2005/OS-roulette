ccc1e13b1199af9f30ee050f28cc84073c792831
amark2005 <amarkeshav14@gmail.com>
2026-02-10 12:38:14 +0530

    init

---
+++ wheel.py
@@ -0,0 +1,40 @@
+import random as rand
+class wheel:
+    data=list()
+    def __init__(self):
+        self.getdata()
+        pass
+    def getdata(self):
+        while True:
+            a=1
+            x=input("Enter the datas: ")
+            if a == 1:
+                self.data.append(x)
+            elif x == None:
+                a=0
+                pass
+wheel()def getdata(self):
+        while True:
+            a=1
+            x=input("Enter the datas: ")
+            if a == 1:
+                self.data.append(x)
+            elif x == None:
+                a=0
+                passdef getdata(self):
+        while True:
+            a=1
+            x=input("Enter the datas: ")
+            if a == 1:
+                self.data.append(x)
+            elif x == None:
+                a=0
+                passdef getdata(self):
+        while True:
+            a=1
+            x=input("Enter the datas: ")
+            if a == 1:
+                self.data.append(x)
+            elif x == None:
+                a=0
+                pass
