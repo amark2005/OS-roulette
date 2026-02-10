96211c63a74db9ad8ccd968bf3383a89757e8af7
amark2005 <amarkeshav14@gmail.com>
2026-02-10 13:17:31 +0530

    tuple fix

--- wheel.py
+++ wheel.py
@@ -4,9 +4,12 @@ class wheel:
     l3=list()
     l3_result=None
     finale=list()
+    l2=list()
+    l2_result=None
     def __init__(self):
         #self.getdata()
-        self.level3()
+        #self.level3()
+        self.level2()
         pass
     def getdata(self):
         while True:
@@ -20,11 +23,19 @@ class wheel:
             self.l3.append(rand.choice(self.data))
         print(self.l3)
         self.l3_result=max(set(self.l3),key=self.l3.count)
-        self.l3_lose=min(set(self.l3),key=self.l3.count)
+        l3_lose=min(set(self.l3),key=self.l3.count)
         self.finale.append(self.l3_result)
-        self.data.remove(self.l3_lose)
+        self.data.remove(l3_lose)
         print(f"Level 3 Winner is {self.l3_result}")
         print(self.data)
+        pass
+    def level2(self):
+        for i in range(0,rand.randint(7,15)):
+            self.l2.append(rand.choice(self.data))
+            print(self.l2)
+            
+            
+        pass
         
         
         
